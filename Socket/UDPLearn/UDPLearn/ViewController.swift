ed23d8cdd1cc24828c6f80cfab6557666513baf3
王智刚 <wangzhigang03@maoyan.com>
2020-08-28 18:59:56 +0800

    udp学习

--- Socket/UDPLearn/UDPLearn/ViewController.swift
+++ Socket/UDPLearn/UDPLearn/ViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import Network
+import SwiftSocket
+
+let host: NWEndpoint.Host = "127.0.0.1"
 
 class ViewController: UIViewController {
 
@@ -14,7 +18,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+class Logger {
+    class Message: CustomStringConvertible, CustomDebugStringConvertible {
+        var description: String {
+            return ""
+        }
+        
+        var debugDescription: String {
+            return ""
+        }
+    }
+}
+
+final class UDP {
+    init() {
+        NWParameters()
+        let fn = NWConnection(host: <#T##NWEndpoint.Host#>, port: <#T##NWEndpoint.Port#>, using: <#T##NWParameters#>)
+    }
+    
+    func send() {
+        
+    }
+}
+
+//MARK: SwiftSocket
 
