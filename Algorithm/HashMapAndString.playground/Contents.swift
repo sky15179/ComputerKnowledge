f6747d1228b9248fa65aea09ff0c11127df1e613
王智刚 <wangzhigang03@maoyan.com>
2020-09-03 13:53:07 +0800

    二维数组的遍历

--- Algorithm/HashMapAndString.playground/Contents.swift
+++ Algorithm/HashMapAndString.playground/Contents.swift
@@ -293,15 +293,41 @@ class LinkList {
 let arr = [1,2,3]
 arr.enumerated()
 
+// Subscript
+extension String {
+    subscript(i: Int) -> String {
+        guard i >= 0 && i < count else { return "" }
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+    
+    subscript(range: Range<Int>) -> String {
+        let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound), limitedBy: endIndex) ?? endIndex
+        let higherIndex = index(lowerIndex, offsetBy: range.upperBound - range.lowerBound, limitedBy: endIndex) ?? endIndex
+        return String(self[lowerIndex..<higherIndex])
+    }
+    
+    subscript(range: ClosedRange<Int>) -> String {
+        let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound), limitedBy: endIndex) ?? endIndex
+        let higherIndex = index(lowerIndex, offsetBy: range.upperBound - range.lowerBound + 1, limitedBy: endIndex) ?? endIndex
+        return String(self[lowerIndex..<higherIndex])
+    }
+}
+
+
 class Solution {
-    private var path: [[Int]] = []
     private var target = ""
 //    private var visited: Set<Position> = Set()
+    private var maxLength = 0
+    private var strs: [String] = []
 
     //矩阵搜索: dfs
     func exist(_ board: [[Character]], _ word: String) -> Bool {
         guard board.count > 0, word.count > 0 else { return false }
-        var strs: [String] = []
+        maxLength = word.count - 1
+        target = word
+        for c in word {
+            strs.append(String(c))
+        }
         var board2 = board
         self.target = word
         for i in 0..<board.count {
@@ -313,28 +339,150 @@ class Solution {
     }
 
     func helper(_ board: inout [[Character]], _ i: Int, _ j: Int, _ k: Int) -> Bool {
-        if i >= board.count { return false}
-
-        //选择
-        target = target + board[i + 1][j]
-        
         //退出条件
-        if target == word {
-            return true
-        }
+        if i >= board.count || i < 0 { return false }
+        if j >= board[0].count || j < 0 { return false }
+        if String(board[i][j]) != strs[k] { return false }
+        if k == maxLength { return true }
 
-        helper(board, i + 1, j)
-        //不选择
-        target = target + board[i][j + 1]
-               //退出条件
-        if target == word {
-            return true
-        }
-        
+        //选择后需要对当前位置占位避免重复
+        let temp = board[i][j]
+        board[i][j] = "/"
+
+
                 //四种变化
         //        i + 1
         //        i - 1
         //        j + 1
         //        j - 1
-        var res = helper(&board, i + 1, j, k + 1) || helper(&board, i - 1, j, k + 1) || helper(&board, i, j + 1, k + 1) || helper(&board, i, j + 1, k + 1)
+        let res = helper(&board, i + 1, j, k + 1) || helper(&board, i - 1, j, k + 1) || helper(&board, i, j + 1, k + 1) || helper(&board, i, j - 1, k + 1)
+        
+        //恢复选择
+        board[i][j] = temp
+        return res
+    }
+}
+
+//表格搜索问题: 基本都是dfs和bfs或者回溯思路解决
+
+class Solution2 {
+    private class Position: Equatable, Hashable {
+        static func == (lhs: Solution2.Position, rhs: Solution2.Position) -> Bool {
+            return lhs.x == rhs.x && lhs.y == rhs.y
+        }
+        
+        var x: Int
+        var y: Int
+        
+        init(x: Int, y: Int) {
+            self.x = x
+            self.y = y
+        }
+        
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(hashValue)
+        }
+        
+        var hashValue: Int {
+            return x * 10 + y
+        }
+    }
+    
+    private var maxSum = 0
+    private var m = 0
+    private var n = 0
+    private var k = 0
+    private var visited: [Position: Bool] = [:]
+    private var visited3: [[Bool]] = []
+    
+    func movingCount(_ m: Int, _ n: Int, _ k: Int) -> Int {
+        guard m > 0, n > 0 else {
+            return 0
+        }
+        guard k > 0 else {
+            return 1
+        }
+        
+        var res = 0
+        var visited2: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: m)
+        var queue: [[Int]] = []
+        queue.append([0, 0, 0, 0])
+        while !queue.isEmpty {
+            let first = queue.removeFirst()
+            let i = first[0]
+            let j = first[1]
+            let si = first[2]
+            let sj = first[3]
+            if (i >= m) || ( j >= n) || (k < si + sj) || visited2[i][j] { continue }
+            visited2[i][j] = true
+            res += 1
+            queue.append([i + 1, j, calculateSum(si + 1), sj])
+            queue.append([i, j + 1, si, calculateSum(sj + 1)])
+        }
+        return res
+    }
+    
+    func movingCount2(_ m: Int, _ n: Int, _ k: Int) -> Int {
+        guard m > 0, n > 0 else {
+            return 0
+        }
+        guard k > 0 else {
+            return 1
+        }
+
+        var queue: [[Int]] = []
+        queue.append([0, 0, 0, 0])
+        
+        for i in 0...m {
+            for j in 0...n {
+                backTrace(i, j)
+            }
+        }
+        //分析：求所有可行解，图的遍历: dfs, bfs
+        //设计：i：横步， j: 纵步
+        //条件： (i > 0 && i < m , j < 0 && j < n), (i + j <= k)
+        //状态变化： (i - 1, 1 + 1 , j - 1, j + 1), ([i][j] = done)
+        let res = visited.values.filter { $0 == true }
+        return res.count
+    }
+
+
+    func backTrace(_ i: Int, _ j: Int) {
+        if visited.keys.contains(Position(x: i, y: j)) { return }
+        if i < 0 || i > m - 1 { return }
+        if j < 0 || j > n - 1 { return }
+        
+        //选择，状态变化，记录
+        visited[Position(x: i, y: j)] = (calculateSum(i) + calculateSum(j)) > k
+        
+        backTrace(i + 1, j)
+        backTrace(i, j + 1)
+    }
+    
+    func calculateSum(_ i: Int) -> Int {
+        var s = 0
+        var cur = i
+        
+        while cur != 0 {
+            s += cur % 10
+            cur = cur / 10
+        }
+        return s
+    }
+    
+    func movingCount3(_ m: Int, _ n: Int, _ k: Int) -> Int {
+        visited3 = Array(repeating: Array(repeating: false, count: n), count: m)
+        self.m = m
+        self.n = n
+        self.k = k
+        let res = dfs(0, 0, 0, 0)
+        return res
+    }
+    
+    
+    func dfs(_ i: Int, _ j: Int, _ si: Int, _ sj: Int) -> Int {
+        if i >= m || j >= n || ((si + sj) >= k) || visited3[i][j] { return 0 }
+        visited3[i][j] = true
+        return 1 + dfs(i + 1, j, (i + 1) % 10 != 0 ? (si + 1) : (si - 8), sj) + dfs(i, j + 1, si, (j + 1) % 10 != 0 ? (sj + 1) : (sj - 8))
+    }
 }
